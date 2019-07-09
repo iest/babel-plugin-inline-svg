9bb3ad4a133e53414c83196a1a804a20e8f80070
Andrew Gower <andrew.gower@oddschecker.com>
2019-07-09 11:24:18 +0100

    Update xlink:href refs on id cleanup

--- optimise.js
+++ optimise.js
@@ -1,10 +1,11 @@
 const Svgo = require('svgo');
 const deasync = require('synchronized-promise');
 
-const makeRegex = str => new RegExp(` +id=\"(${str})\"`, 'gi');
+const makeDefinitionRegex = str => new RegExp(` +id=\"(${str})\"`, 'gi');
+const makeReferenceRegex = str => new RegExp(` +xlink:href=\"#(${str})\"`, 'gi');
 
-const idRegex = makeRegex('.+');
-const getIds = str => (str.match(idRegex) || []).map(s => s.replace(idRegex, '$1'));
+const idDefRegex = makeDefinitionRegex('.+');
+const getIds = str => (str.match(idDefRegex) || []).map(s => s.replace(idDefRegex, '$1'));
 
 module.exports = function optimise(name, content, opts) {
   const ids = getIds(content);
@@ -28,9 +29,13 @@ module.exports = function optimise(name, content, opts) {
     let { data: output } = optimizeSync(content);
 
     ids.forEach(id => {
-      const r = makeRegex(id);
-      const replacement = ` id="${name}-${id}"`;
-      output = output.replace(r, replacement);
+      const defReg = makeDefinitionRegex(id);
+      const refReg = makeReferenceRegex(id);
+      const newId = `${name}-${id}`;
+      const defReplacement = ` id="${newId}"`;
+      const refReplacement = ` xlink:href="#${newId}"`;
+      output = output.replace(defReg, defReplacement);
+      output = output.replace(refReg, refReplacement)
     });
 
     const withIds = output.replace();
